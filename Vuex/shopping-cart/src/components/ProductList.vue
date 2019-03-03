822cda28dbe24479f0880a2cfcfc45def31eca95
Tien Pham <phamtien1709@gmail.com>
2019-03-03 23:46:35 +0700

    update

---
+++ Vuex/shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li
+        v-for="product in products"
+        v-bind:key="product.inventory"
+      >{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import shop from "@/api/shop";
+
+export default {
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    shop.getProducts(products => {
+      this.products = products;
+    });
+  }
+};
+</script>
